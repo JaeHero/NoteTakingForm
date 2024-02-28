934e4078fe3187424e446a20da0d27ce4d028484
Your Name <you@example.com>
2024-02-27 21:10:55 -0500

    Add project files.

---
+++ NoteTakingForm/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NoteTakingForm
+{
+    public partial class Form1 : Form
+    {
+        DataTable table;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            table = new DataTable();
+            table.Columns.Add("Title", typeof(String));
+            table.Columns.Add("Messages", typeof(String));
+
+            dataGridView1.DataSource = table;
+
+            dataGridView1.Columns["Messages"].Visible = false;
+            dataGridView1.Columns["Title"].Width = 265;
+        }
+
+        private void New_Click(object sender, EventArgs e)
+        {
+            txtTitle.Clear();
+            txtMessage.Clear();
+        }
+
+        private void bttSave_Click(object sender, EventArgs e)
+        {
+            // Search for an existing title in the DataTable
+            var existingRow = table.AsEnumerable().FirstOrDefault(row => row.Field<String>("Title") == txtTitle.Text);
+
+            if (existingRow != null)
+            {
+                // If title exists, update the message
+                existingRow.SetField("Messages", txtMessage.Text);
+            }
+            else
+            {
+                // If title doesn't exist, add a new row
+                table.Rows.Add(txtTitle.Text, txtMessage.Text);
+            }
+
+            txtTitle.Clear();
+            txtMessage.Clear();
+        }
+
+        private void bttRead_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;
+
+            if(index > -1)
+            {
+                txtTitle.Text = table.Rows[index].ItemArray[0].ToString();
+                txtMessage.Text = table.Rows[index].ItemArray[1].ToString();
+
+
+            }
+        }
+
+        private void bttDelete_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;
+            table.Rows[index].Delete();
+        }
+    }
+}
